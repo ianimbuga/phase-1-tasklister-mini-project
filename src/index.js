9d8371cb223885a26fb49dc7158bb210d495186c
ianimbuga <imbugaian20@gmail.com>
2024-10-17 18:53:37 +0300

    code challenge 2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  let form = document.querySelector('#create-task-form')
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let todo = document.getElementById('new-task-description').value
-    buildToDo(todo)
-    form.reset()
-  })
-
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      createTodo(e.target["new-task-description"].value);
+      form.reset();
+  });
 });
 
-function buildToDo(todo) {
-  let li = document.createElement('li')
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.textContent = 'x'
-  li.textContent = `${todo}`
-  li.appendChild(btn)
-  console.log(li)
-  document.querySelector('#tasks').appendChild(li)
+function createTodo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "Delete";
+  li.textContent = todo; 
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
 }
 
 function handleDelete(e) {
-  e.target.parentNode.remove()
+  e.target.parentNode.remove();
 }
